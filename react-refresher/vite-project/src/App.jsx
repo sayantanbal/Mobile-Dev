22ee08ee144ef34b45c6061dd469382a475a8dd8
sayantanbal <balsayantan546@gmail.com>
2026-05-03 12:45:01 +0530

    Refactor App component to implement dark mode toggle and optimize useEffect hooks

--- react-refresher/vite-project/src/App.jsx
+++ react-refresher/vite-project/src/App.jsx
@@ -1,12 +1,72 @@
-import H1 from "./H1";
+
+// import { useState } from "react";
+
+// function App() {
+//   const [state, setState] = useState(0); // top level state, that means it will be shared across all the components, and when it changes, it will cause all the components to re-render, even if they don't use that state, which is not efficient.
+
+//   return (
+//     <>
+//       <h1>{state}</h1>
+//       <button onClick={() => setState(state + 1)}>+</button>
+//       <button onClick={() => setState(state - 1)}>-</button>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+import { use, useEffect, useState } from "react";
+
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // useEffect w/o dependency array
+  //  will run after every render, which is not efficient, because it will cause unnecessary side effects, and it will also cause infinite loops if the side effect changes the state that causes the component to re-render.
+  useEffect(() => {
+    console.log("useEffect 1 called")
+  });
+
+
+  // useEffect with empty dependency array
+  // will run only once, after the first render, which is efficient, because it will not cause unnecessary side effects, and it will not cause infinite loops.
+  useEffect(() => {
+    console.log("useEffect 2 called")
+  }, []);
+
+
+  // useEffect with dependency array
+  // will run only when the dependencies change, which is efficient, because it will not cause unnecessary side effects, and it will not cause infinite loops.
+  useEffect(() => {
+    console.log("useEffect 3 called")
+  }, [isDarkMode]);
+
   return (
-    <>
-      <H1 title="{Student Names}" desc="hello i am desc-1" subheading="Subheading-1" />
-      <H1 title="Heading-2" desc="hello i am desc-2" />
-      <H1 title="Heading-3" />
-    </>
+    <div 
+    style = {{
+        backgroundColor: isDarkMode ? "black" : "white",
+        color: isDarkMode ? "white" : "black",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "200rem",
+      }}
+    >
+     <button onClick={() => setIsDarkMode(!isDarkMode)}>
+      Toggle theme
+      </button>
+      
+      
+    </div>
   );
 }
 
